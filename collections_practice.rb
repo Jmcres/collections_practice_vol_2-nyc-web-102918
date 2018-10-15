eea92a1d6568e422737f551ed5e7e6a7a2e061b7
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-15 19:19:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,3 +8,12 @@ end
 def contain_a (array)
   array.select {|element| element.include?("a")}
 end
+
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+
+
+def remove_non_strings(array)
+  array.reject {|element| element.class != String}
+end
